9b638e30315ece7defaf1c3dbb3aa4dfe8d881dd
infernal-slam@homai;.com <infernal-slam@hotmail.com>
2015-10-29 12:09:43 -0700

    test insert

--- public/todo.js
+++ public/todo.js
@@ -1,4 +1,16 @@
 angular.module('todoApp', [])
   .controller('TodoListController', function() {
+    var todoList = this
+
+     todoList.name ='tak'
     
+     todoList.show =[]
+
+     todoList.add = function (input){
+        var data_input = {
+            number1 : input.n1
+        }
+        todoList.show.push(data_input)
+     }
+
   });
