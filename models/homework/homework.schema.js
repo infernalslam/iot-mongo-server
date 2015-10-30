ea2fd5120b7aac83fc9616764c5cd84b4612faa4
infernal-slam@homai;.com <infernal-slam@hotmail.com>
2015-10-30 03:41:58 -0700

    comment code

--- models/homework/homework.schema.js
+++ models/homework/homework.schema.js
@@ -17,6 +17,36 @@
   })
 
   /*test delete*/
+  /*
+      // get the user starlord55
+      User.find({ username: 'starlord55' }, function(err, user) {
+        if (err) throw err;
+
+        // delete him
+        user.remove(function(err) {
+          if (err) throw err;
+
+          console.log('User successfully deleted!');
+        });
+      });
+      ---------------------------------------
+            // find the user with id 4
+      User.findOneAndRemove({ username: 'starlord55' }, function(err) {
+        if (err) throw err;
+
+        // we have deleted the user
+        console.log('User deleted!');
+      });
+      ----------------------------------------
+      // find the user with id 4
+      User.findByIdAndRemove(4, function(err) {
+        if (err) throw err;
+
+        // we have deleted the user
+        console.log('User deleted!');
+      });
+
+  */
   
 
 
